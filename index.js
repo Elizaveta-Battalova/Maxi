4f240c3a32bfb97afb5882b808dd2691bf95fd7b
Liza <elizaveta.battalova@rambler.ru>
2021-11-27 01:50:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function hideButton() {
+    let item = document.getElementById("btn");
+    if (item.style.display == "block") item.style.display = "none";
+    else item.style.display = "block";
+  }
